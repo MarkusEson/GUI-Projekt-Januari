cfc452094f2fb42650443b6ffc258ba124e829f1
Unknown <josef_skywakman@hotmail.com>
2017-09-25 11:23:10 +0200

    Auto stash before merge of "Josef" and "master"

--- JanuariYahtzee/gamebrain.h
+++ JanuariYahtzee/gamebrain.h
@@ -1,11 +1,58 @@
 #ifndef GAMEBRAIN_H
 #define GAMEBRAIN_H
 
-// #include <iostream>
 #include <algorithm>
 #include <QDebug>
 #include <stdlib.h>
 #include <time.h>
+
+enum class WIPenum {
+    rolldice = 0,
+    ones,
+    twos,
+    threes,
+    fours,
+    fives,
+    sixes,
+
+    sum,
+    bonus,
+
+    onepair,
+    twopairs,
+    threeofakind,
+    fourofakind,
+    fullhouse,
+    smallstraight,
+    largestraight,
+    yahzee,
+    chance,
+    yahzeebonus,
+
+    total,
+
+
+};
+
+inline int intFromKey(Key key) {
+    return int(key);
+}
+
+class Die {
+public:
+    Die();
+    ~Die();
+
+    void setValue(int newValue);
+    int getValue();
+    bool checkIsChecked();
+    void checkDie();
+
+private:
+    int _value = 6;
+    bool _isChecked = 0;
+
+};
 
 class GameBrain
 {
@@ -14,10 +61,29 @@ public:
     ~GameBrain();
 
     void rollDice();
-    int *getDiceArray();
+    Die *getDiceArray();
+    void checkDie(int dieNumber);
+
+    void oneToSix();
+    void xOfAKind();
+    void fullHouse();
+    void smallLargeStraight();
+
+    QString getScoreFromArray();
+    QString calculateScoreBoard(int player, int sumBonusOrTotal);
+
 
 private:
-    int diceArray[5];
+    Die _diceArray[5];
+
+    int throwValue [5] = {2,3,4,5,6};           //testvariabler för funktionerna
+    int dieValue = 4;
+    int sum = 0;
+    int dieSort = 3;
+    int smallLarge = 2;
+
+
+    int _scoreArray[19][4];
 };
 
 #endif // GAMEBRAIN_H
